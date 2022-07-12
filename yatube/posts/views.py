0a8759129f41337621701b4e34e7b67d88bf1e67
Inan Meshkov <obdultipov@yandex.ru>
2022-07-12 14:51:40 +0600

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def groups(request):
+    return HttpResponse('Страницы сообществ')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Страница сообщества {slug}')
