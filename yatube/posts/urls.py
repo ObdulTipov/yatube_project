c0e9af84219b8596bb954e301805e010664681a3
Иван Мешков <ObdulTipov@yandex.ru>
2022-07-14 11:10:05 +0600

    Разбираем шаблоны: include, block, extends

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Главная страница
     path('', views.index),
-    path('group/', views.group_list),
+    # Страница сообщества
+    path('group/', views.groups),
+    # Страница сообщества {slug}. Ждем пременную типа slug,
+    # и будем использовать ее под именем slug
     path('group/<slug:slug>/', views.group_posts),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def groups(request):
     return HttpResponse('Страницы сообществ')
 
 
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
     return HttpResponse(f'Страница сообщества {slug}')
