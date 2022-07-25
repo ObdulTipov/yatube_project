2ecb88dfc466a3d6d3c0f425c1f1335cd7220651
Иван Мешков <ObdulTipov@yandex.ru>
2022-07-25 10:54:33 +0600

    Словарь контекстаб ветвления циклы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
 
 
 def groups(request):
@@ -13,4 +18,10 @@ def groups(request):
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
-    return HttpResponse(f'Страница сообщества {slug}')
+    template = 'posts/group_list.html'
+    title = 'Лев Толстой – зеркало русской революции.'
+    context = {
+        'title': title,
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
