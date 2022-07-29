038a7d74542c27555efc60ebed83006b74cb00ca
Иван Мешков <ObdulTipov@yandex.ru>
2022-07-29 11:57:10 +0600

    ORM и вывод из базы данных

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,12 +17,10 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.object.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     title = 'Лев Толстой – зеркало русской революции.'
-    text = 'Здесь будет информация о группах проекта Yatube'
     context = {
         'title': title,
-        'text': text,
         'group': group,
         'posts': posts,
     }
