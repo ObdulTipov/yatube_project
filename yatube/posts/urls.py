cc340a78450411125edc334f7ec7a2268945c4fb
Иван Мешков <ObdulTipov@yandex.ru>
2022-07-29 11:05:10 +0600

    ORM и вывод из базы данных

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,11 +5,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    # Главная страница
-    path('posts/', views.index, name='posts_list'),
-    # Страница сообщества
-    path('group/', views.group_list, name='group_list'),
-    # Страница сообщества {slug}. Ждем пременную типа slug,
-    # и будем использовать ее под именем slug
-    path('group/<slug:slug>/', views.group_posts_list, name='group_posts_list'),
+    path('', views.index, name='index'),
+    path('group_list/', views.group_posts, name='group_posts')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,27 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post, Group
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Главная страница Yatube'
+    text = 'Последние обновления на сайте'
     context = {
         'title': title,
-        'text': 'Это главная страница проекта Yatube'
+        'text': text,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context) 
 
 
-def groups(request):
-    return HttpResponse('Страницы сообществ')
-
-
-# В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.object.filter(group=group).order_by('-pub_date')[:10]
     title = 'Лев Толстой – зеркало русской революции.'
+    text = 'Здесь будет информация о группах проекта Yatube'
     context = {
         'title': title,
-        'text': 'Здесь будет информация о группах проекта Yatube'
+        'text': text,
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
