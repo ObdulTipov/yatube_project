3fa2064a29c4b56c46b34e3ae675a26f7a2a7879
Иван Мешков <ObdulTipov@yandex.ru>
2022-08-01 12:53:54 +0600

    после ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+LAST_TEN_POSTS = 10
+
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
@@ -22,7 +24,11 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
-        null=True
+        null=True,
+        related_name='posts'
     )
+
+    class Meta:
+        ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-# Импортируем модель, чтобы обратиться к ней
-from .models import Post, Group
+from .models import LAST_TEN_POSTS, Post, Group
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:LAST_TEN_POSTS]
     title = 'Главная страница Yatube'
     text = 'Последние обновления на сайте'
     context = {
@@ -12,15 +11,13 @@ def index(request):
         'text': text,
         'posts': posts,
     }
-    return render(request, 'posts/index.html', context) 
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    title = 'Лев Толстой – зеркало русской революции.'
+    posts = Post.objects.select_related('group')[:LAST_TEN_POSTS]
     context = {
-        'title': title,
         'group': group,
         'posts': posts,
     }
